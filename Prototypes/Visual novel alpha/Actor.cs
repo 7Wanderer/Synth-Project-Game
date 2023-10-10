36b67d743cdaf65890ac7cbb774d39599cc46b9a
7Wanderer <khanahyan.022@gmail.com>
2023-10-10 21:33:55 +0100

    Created class Actor

---
+++ Prototypes/Visual novel alpha/Actor.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct3D9;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visual_novel_alpha
+{
+    enum TextureSheet
+    {
+        IdleAnim,
+        RunningAnim
+    }
+    enum Portraits
+    {
+        Normal,
+        Happy,
+        Joyous,
+        Curious,
+        Confident,
+        Distressed,
+        Shocked,
+        Sad,
+        Crying,
+        Angry,
+        Grimacing,
+        Thinking,
+        Custom1,
+        Custom2
+    }
+    public class Actor
+    {
+        //constants (had to define with readonly because i'm a bitch)
+        readonly int SPRITE_WIDTH = 480, SPRITE_HEIGHT=960;
+        readonly float SPRITE_SCALE = 1;
+        readonly int PORTRAIT_WIDTH = 0, PORTRAIT_HEIGHT;
+        readonly float PORTRAIT_SCALE = 1;
+
+        readonly Vector2 DEFAULT = new(80,80);
+        readonly Vector2 DEFAULT_REVERSE = new(1360,80);
+
+        TextureSheet animation = TextureSheet.IdleAnim;
+
+        Portraits portrait = Portraits.Normal;
+
+        Texture2D[] spriteTextureSheets;
+        Texture2D portraitTextureSheet;
+
+        Vector2 spritePosition;
+        Vector2 portraitPosition;
+
+        SpriteEffects isReversed;
+        SpriteEffects portraitReversed;
+
+        bool portraitHidden = true;
+
+        public Actor(Texture2D portraitTextureSheet) 
+        { 
+            // this.spritePosition = spritePosition;
+            // this.spriteTextureSheets = spriteTextureSheets;
+            this.portraitTextureSheet = portraitTextureSheet;
+            portraitPosition = DEFAULT;
+        }
+
+        public void Update()
+        {
+
+        }
+        Rectangle getRectFromTexture()
+        {
+            return new Rectangle((int)portrait * PORTRAIT_WIDTH, 0,
+                                PORTRAIT_WIDTH, PORTRAIT_HEIGHT);
+        }
+        Rectangle getRectFromPortrait()
+        {
+            return new Rectangle((int)portrait * PORTRAIT_WIDTH,0,
+                                PORTRAIT_WIDTH,PORTRAIT_HEIGHT);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            /*
+            spriteBatch.Draw(spriteTextureSheets[((int)animation)],
+                spritePosition,
+                getRectFromTexture(),
+                Color.White, 0, new Vector2(), SPRITE_SCALE, isReversed, 0);
+            */
+            if(!portraitHidden)
+                spriteBatch.Draw(portraitTextureSheet,
+                    portraitPosition,
+                    getRectFromPortrait(),
+                    Color.White, 0, new Vector2(), SPRITE_SCALE, isReversed, 0);
+        }
+    }
+}
--- Prototypes/Visual novel alpha/Game1.cs
+++ Prototypes/Visual novel alpha/Game1.cs
@@ -9,6 +9,10 @@ namespace Visual_novel_alpha
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Actor player1;
+
+        public SpriteFont gameFont;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,13 +23,20 @@ namespace Visual_novel_alpha
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            _graphics.PreferredBackBufferHeight = 1080;
+            _graphics.PreferredBackBufferWidth = 1920;
+            _graphics.ApplyChanges();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Texture2D portraitAlpha = Content.Load<Texture2D>("Syn neutral");
+            gameFont = Content.Load<SpriteFont>("defaultFont");
+
+            player1 = new(portraitAlpha);
 
             // TODO: use this.Content to load your game content here
         }
@@ -43,6 +54,12 @@ namespace Visual_novel_alpha
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            
+            player1.Draw(_spriteBatch);
+            
+            _spriteBatch.End();
 
             // TODO: Add your drawing code here
 
